20c4987647b6a6e53e96352568e705eddab0a08c
Eduardo Gonzalez <l17390753@chetumal.tecnm.mx>
2021-06-03 16:59:29 -0500

    Resolviendo errores segunda parte evaluacion

--- app/src/main/java/com/example/rfcgenerator/MainActivity.kt
+++ app/src/main/java/com/example/rfcgenerator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.rfcgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Month
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        date.setOnClickListener{ showDatePickerDialog() }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+
+    fun onDateSelected(day: Int, month: Int, year: Int) {
+        date.setText("$day/$month/$year")
     }
 }
